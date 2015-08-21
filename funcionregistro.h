c903a89fd1be93baa1646df7ac9cd2069a4da3ad
camihera333 <camihera333@hotmail.com>
2015-08-21 18:20:39 -0500

    De acuerdo a los archivos sumimistrados por el docente se realiza la modificación de este código, se incluyen la librerias y funciones necesarias y se cambia la funcion original llamada registros por la funcion mostrarregistros, la cual tiene como argumento un puntero a un entero de 32 bits sin signo (uint32_t), llamado registros y una variable tipo size_t llamada t.

--- funcionregistro.h
+++ funcionregistro.h
@@ -1,1 +1,14 @@
-long int registros(long int *direc);
+#ifndef _FUNCIONREGISTRO_H_
+#define	_FUNCIONREGISTRO_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <windows.h>
+
+#include "colors.h"
+
+void mostrarregistros(uint32_t *registros, size_t t);
+
+#endif
+
