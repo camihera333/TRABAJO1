8df5e6ff5e519a57eff2dac57ef1cbf747799414
camihera333 <camihera333@hotmail.com>
2015-08-21 17:15:10 -0500

    Adicional a los datos que tenía este archivo, se agrega un arreglo del tipo long int de 6 elementos, en el cual se almacenará el resultado de 6 operaciones aritmético lógicas que podrán ser entre los valores de las posiciones del registro o entre valores enteros arbitrarios, las funciones enunciadas en este archivo son ADD,AND,EOR,MOV,ORR,SUB

--- main.c
+++ main.c
@@ -5,6 +5,18 @@
 main()
 {
 
-    long int R[13]={0,0,0,0,0,0,0,0,0,0,0,0,0};    
-    registros(R);    
+    long int R[13]={0,0,0,0,0,0,0,0,0,0,0,0,0};
+    long int Rdr[6];
+    long int Rn,Rm;
+    registros(R);
+    Rdr[0]=ADD(Rn,Rm);
+    Rdr[1]=AND(Rn,Rm);
+    Rdr[2]=EOR(Rn,Rm);
+    Rdr[3]=MOV(Rm);
+    Rdr[4]=ORR(Rn,Rm);
+    Rdr[5]=SUB(Rn,Rm);
+
+
+
+
 }
