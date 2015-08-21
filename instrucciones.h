223336a99c3193c199d75c4ec7a103a6edd08949
camihera333 <camihera333@hotmail.com>
2015-08-21 11:48:01 -0500

    En este archivo se encuentran los prototipos de las funciones ADD,AND,EOR,ORR,SUB, las cuales son del tipo long int y poseen dos argumentos correspondientes a los operandos de la operación a realizar en cada caso, dichos argumentos se denotan como Rn y Rm, adicional a estas funciones se encuentra la función MOV que es del mismo tipo pero varía en que sólo tiene un argumento de tipo long int (Rm).

---
+++ instrucciones.h
@@ -0,0 +1,6 @@
+long int ADD ( long int Rn, long int Rm);
+long int AND (long int Rn, long int Rm);
+long int EOR (long int Rn, long int Rm);
+long int MOV (long int Rm);
+long int ORR (long int Rn, long int Rm);
+long int SUB (long int Rn, long int Rm);
