1aabdc2b6c229a4351a747d8f425efbee2aebd07
camihera333 <camihera333@hotmail.com>
2015-08-29 05:15:07 -0500

    Se agregaron los prototipos de funciones relacionadas con las instruciones de desplazamiento, en total las funciones enunciadas en este texto son ADC,ADD,AND,ASR,BISe agregaron los prototipos de funciones relacionadas con las instruc… …ciones de desplazamiento, en total las funciones enunciadas en este texto son ADC,ADD,AND,ASR,BIC,CMN,CMP,EOR,LSL,LSR,MOV,MUL,MVN,NOP,ORR,REV,REV16,REVSH,ROR,RSB,SBC,SUB Y TSTC,CMN,CMP,EOR,LSL,LSR,MOV,MUL,MVN,NOP,ORR,REV,REV16,REVSH,ROR,RSB,SBC,SUB Y TST

--- instrucciones.h
+++ instrucciones.h
@@ -2,27 +2,51 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <windows.h>
-
 #include "colors.h"
 
-/**
-*\brief funcion ADD
-*\param long int Rn operando 1 de la suma
-*\param long int Rm operando 2 de la suma
-*\return 0
-*/
+
 void ADD (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
-
-/**
-*\brief funcion AND
-*\param long int Rn operando 1 de la multiplicacion logica
-*\param long int Rm operando 2 de la multiplicacion logica
-*\return 0
-*/
-
 void AND (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
 void EOR (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
 void MOV (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
 void ORR (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
-void SUB (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void SUB (uint32_t Rn, uint32_t Rm,uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void MVN (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void RSB (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void MUL (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void BIC (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void NOP ();
+void SBC (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void CMN (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void CMP (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void LSL (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void LSR (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void ADC (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void ASR(uint32_t Rm,uint32_t corri,uint32_t* Rdr, uint32_t* registros, uint32_t* Banderas);
+void REV (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void REV16 (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void REVSH (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void ROR(uint32_t Rm,uint32_t corri,uint32_t* Rdr, uint32_t* registros, uint32_t* Banderas);
+void TST (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+/*void ASR (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+//void ROR (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas,uint32_t* bitsRn, uint32_t* bitsRm);
+
+void CMN (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void CMP (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+
+void LSL (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void LSR (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+
+
+
+
+
+void REV (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void REV16 (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+void REVSH (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);
+
+
+
+void TST (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas);*/
+
 
