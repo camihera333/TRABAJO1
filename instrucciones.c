b43129a2c30d2c5b5e91c0bd6e8e50233d3a8d5d
camihera333 <camihera333@hotmail.com>
2015-08-29 05:16:40 -0500

    Se anexó a este archivo las funciones correspondientes a las instrucciones de desplazamiento con sus respectivas banderas y cada uno de los procedimientos

--- instrucciones.c
+++ instrucciones.c
@@ -1,11 +1,40 @@
-
+#include "funcionregistro.h"
 #include "instrucciones.h"
 #include <stdint.h>
 
 
+void convbits (uint32_t Rn, uint32_t Rm, uint32_t* Registros, uint32_t* bitsRn, uint32_t* bitsRm)
+{
+    int i;
+    for(i=31;i>=0;i--)
+    {
+     bitsRn[i]=*(Registros+Rn)%2;
+    *(Registros+Rn)=(*(Registros+Rn))/2;
+    }
+        for(i=0;i<=31;i++)
+    {
+        printf("%d",bitsRn[i]);
+    }
+
+printf("\n\n");
+
+
+    for(i=31;i>=0;i--)
+    {
+     bitsRm[i]=*(Registros+Rm)%2;
+    *(Registros+Rm)=(*(Registros+Rm))/2;
+    }
+    for(i=0;i<=31;i++)
+    {
+        printf("%d",bitsRm[i]);
+    }
+}
+
  void ADD (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
 {
+
     *(Rdr+0)=*(Registros+Rn)+*(Registros+Rm);
+
     if(((1<<7)&*(Rdr+0))==128)
 
         *(Banderas+0)=1;
@@ -31,11 +60,7 @@
     else
         *(Banderas+3)=0;
 
-    mostraralu(Rdr,0);
-    mostrarbanderas(Banderas,4);
 }
-
-
 
 void AND (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
 {
@@ -65,8 +90,6 @@ void AND (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t
     else
         *(Banderas+3)=0;
 
-    mostraralu(Rdr,1);
-    mostrarbanderas(Banderas,4);
 }
 
 void EOR (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
@@ -97,8 +120,6 @@ void EOR (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t
     else
         *(Banderas+3)=0;
 
-    mostraralu(Rdr,2);
-    mostrarbanderas(Banderas,4);
 }
 
 void MOV (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
@@ -129,8 +150,6 @@ void MOV (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
     else
         *(Banderas+3)=0;
 
-    mostraralu(Rdr,3);
-    mostrarbanderas(Banderas,4);
 }
 
 void ORR (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
@@ -160,14 +179,11 @@ void ORR (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t
      *(Banderas+3)=1;
     else
         *(Banderas+3)=0;
-
-    mostraralu(Rdr,4);
-    mostrarbanderas(Banderas,4);
-
 }
 
 void SUB (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
 {
+    *(Rdr+5)=*(Registros+Rn)-*(Registros+Rm);
     if(((1<<7)&*(Rdr+5))==128)
 
         *(Banderas+0)=1;
@@ -192,8 +208,581 @@ void SUB (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t
      *(Banderas+3)=1;
     else
         *(Banderas+3)=0;
-
-    mostraralu(Rdr,5);
-    mostrarbanderas(Banderas,4);
 }
+
+void MVN (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+      *(Rdr+6)=~*(Registros+Rm);
+
+    if(((1<<7)&*(Rdr+6))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+6)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if(((Rm>=128)&(*(Rdr+6)<128)) |(((Rm<128))&(*(Rdr+6)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+}
+
+void RSB (uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+    *(Rdr+7)=~*(Registros+Rm);
+    *(Rdr+7)=*(Rdr+7)-1;
+    *(Rdr+3)=*(Registros+Rm);
+    if(((1<<7)&*(Rdr+7))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+7)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if(((Rm>=128)&(*(Rdr+7)<128)) |(((Rm<128))&(*(Rdr+7)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+
+}
+void MUL (uint32_t Rn,uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+    *(Rdr+8)=(*(Registros+Rm))*(*(Registros+Rn));
+
+     if(((1<<7)&*(Rdr+8))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+8)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(Registros+Rn)+*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if((((Rn>=128)&(Rm>=128))&(*(Rdr+8)<128)) |(((Rn<128)&(Rm<128))&(*(Rdr+8)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+}
+
+void BIC (uint32_t Rn,uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+    *(Rdr+9)=(*(Registros+Rn))&(~*(Registros+Rm));
+    if(((1<<7)&*(Rdr+9))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+9)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(Registros+Rn)+*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if((((Rn>=128)&(Rm>=128))&(*(Rdr+9)<128)) |(((Rn<128)&(Rm<128))&(*(Rdr+9)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+}
+
+void NOP ()
+{
+}
+
+void SBC (uint32_t Rn,uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+    *(Rdr+10)=~*(Registros+Rn);
+    *(Rdr+10)=*(Rdr+10)-1;
+    *(Rdr+10)=*(Rdr+10)+*(Registros+Rm);
+    if(((1<<7)&*(Rdr+10))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+10)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(Registros+Rn)+*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if((((Rn>=128)&(Rm>=128))&(*(Rdr+10)<128)) |(((Rn<128)&(Rm<128))&(*(Rdr+10)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+}
+
+void CMN (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+    int aux=0;
+    aux=*(Registros+Rn)+*(Registros+Rm);
+
+    if(((1<<7)&aux)==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(aux==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(Registros+Rn)+*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if((((Rn>=128)&(Rm>=128))&(aux<128)) |(((Rn<128)&(Rm<128))&(aux>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+
+}
+
+void CMP (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+    int aux1=0;
+    aux1=*(Registros+Rn)-*(Registros+Rm);
+
+    if(((1<<7)&aux1)==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(aux1==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(Registros+Rn)+*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if((((Rn>=128)&(Rm>=128))&(aux1<128)) |(((Rn<128)&(Rm<128))&(aux1>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+
+}
+
+void LSL (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+    *(Rdr+12)=*(Registros+Rn)<<*(Registros+Rm);
+
+    if(((1<<7)&*(Rdr+12))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+13)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(Registros+Rn)+*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if((((Rn>=128)&(Rm>=128))&(*(Rdr+12)<128)) |(((Rn<128)&(Rm<128))&(*(Rdr+12)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+
+}
+void TST (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+    int aux=0;
+    aux=*(Registros+Rn)&*(Registros+Rm);
+
+    if(((1<<7)&aux)==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(aux==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(Registros+Rn)+*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if((((Rn>=128)&(Rm>=128))&(aux<128)) |(((Rn<128)&(Rm<128))&(aux>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+
+}
+
+void LSR (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+    *(Rdr+14)=*(Registros+Rn)>>*(Registros+Rm);
+
+    if(((1<<7)&*(Rdr+14))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+14)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(Registros+Rn)+*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if((((Rn>=128)&(Rm>=128))&(*(Rdr+14)<128)) |(((Rn<128)&(Rm<128))&(*(Rdr+14)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+
+}
+void REV(uint32_t Rm,uint32_t* Rdr,uint32_t* registros, uint32_t* Banderas)
+{
+    uint32_t help[4],A[4]={0,0,0,0};
+    int i=0;
+    for(i=0;i<32;i++)
+    {
+        if((i<8)&&(i>=0))
+        {
+          A[0]=(A[0]|1<<i);
+        }
+        if((i<16)&&(i>=8))
+        {
+          A[1]=(A[1]|1<<i);
+        }
+        if((i<24)&&(i>=16))
+        {
+          A[2]=(A[2]|1<<i);
+        }
+        if((i<32)&&(i>=24))
+        {
+          A[3]=(A[3]|1<<i);
+        }
+    }
+    help[0]=(*(registros+Rm)&A[0]);
+    help[1]=(*(registros+Rm)&A[1]);
+    help[2]=(*(registros+Rm)&A[2]);
+    help[3]=(*(registros+Rm)&A[3]);
+    *(Rdr+15)=((help[0]<<24)|(help[1]<<16)|(help[2]>>8)|(help[3]>>24));
+
+    *(Rdr+15)=*(registros+Rm);
+    if(((1<<7)&*(Rdr+15))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+15)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if(((Rm>=128)&(*(Rdr+15)<128)) |(((Rm<128))&(*(Rdr+15)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+}
+
+void REV16(uint32_t Rm,uint32_t* Rdr,uint32_t* registros,uint32_t* Banderas)
+{
+    uint32_t help[2],A[2]={0,0};
+    int i=0;
+    for(i=0;i<32;i++)
+    {
+        if((i<16)&&(i>=0))
+        {
+          A[0]=(A[0]|1<<i);
+        }
+        if((i<32)&&(i>=16))
+        {
+          A[1]=(A[1]|1<<i);
+        }
+    }
+    help[0]=(*(registros+Rm)&A[0]);
+    help[1]=(*(registros+Rm)&A[1]);
+    *(Rdr+16)=((help[0]<<16)|(help[1]>>16));
+    *(Rdr+16)=*(registros+Rm);
+    if(((1<<7)&*(Rdr+16))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+16)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if(((Rm>=128)&(*(Rdr+16)<128)) |(((Rm<128))&(*(Rdr+16)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+}
+
+void REVSH(uint32_t Rm,uint32_t* Rdr,uint32_t* registros, uint32_t* Banderas)
+{
+    uint32_t help,A[2]={0,0},total=0;
+    int i=0;
+    for(i=8;i<32;i++)
+    {
+       if ((i>=8)&&(i<16))
+       A[0]=(A[0]|1<<i);
+       if ((i>=8)&&(i<32))
+       A[1]=(A[1]|1<<i);
+    }
+    help=(*(registros+Rm)&A[0]);
+    total=(help>>8);
+    if (total>=128)
+    {*(Rdr+17)=A[1]|total;}
+    else
+    {*(Rdr+17)=A[1];}
+    *(Rdr+17)=*(registros+Rm);
+    if(((1<<7)&*(Rdr+17))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+17)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if(((Rm>=128)&(*(Rdr+17)<128)) |(((Rm<128))&(*(Rdr+17)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+}
+
+void ROR(uint32_t Rm,uint32_t Rn,uint32_t* Rdr, uint32_t* registros, uint32_t* Banderas)
+{
+    int help,i;
+    uint32_t A=0,help2;
+    help=32-Rn;
+    for(i=0;i<32;i++)
+    {
+        if((i>=0)&&(i<=Rn))
+            A=A|(1<<i);
+    }
+    help2=*(registros+Rm)&A;
+    *(Rdr+18)=(help2<<help)|(*(registros+Rm)>>Rn);
+
+    *(Rdr+18)=*(registros+Rm);
+    if(((1<<7)&*(Rdr+18))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+18)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if(((Rm>=128)&(*(Rdr+18)<128)) |(((Rm<128))&(*(Rdr+18)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+}
+
+void ASR(uint32_t Rn,uint32_t Rm,uint32_t* Rdr, uint32_t* registros, uint32_t* Banderas)
+{
+    int help,i;
+    uint32_t A=0;
+    help=32-Rn;
+    for(i=0;i<32;i++)
+    {
+        if((i>=0)&&(i<=Rn))
+            A=A|(1<<i);
+    }
+
+    *(Rdr+19)=(help<<help)|(*(registros+Rn)>>Rm);
+    *(Rdr+19)=*(registros+Rn);
+
+    if(((1<<7)&*(Rdr+19))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+19)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+    uint32_t c;
+    c=*(registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if(((Rm>=128)&(*(Rdr+19)<128)) |(((Rm<128))&(*(Rdr+19)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+}
+
+void ADC (uint32_t Rn, uint32_t Rm, uint32_t* Rdr, uint32_t* Registros, uint32_t* Banderas)
+{
+    uint32_t c;
+    c=*(Registros+Rn)+*(Registros+Rm);
+
+    if(c>=255){
+    *(Banderas+2)=1;
+    *(Rdr+19)=c+1;
+    }
+    else {
+        *(Banderas+2)=0;
+        *(Rdr+19)=c;
+    }
+
+    if(((1<<7)&*(Rdr+19))==128)
+
+        *(Banderas+0)=1;
+    else
+        *(Banderas+0)=0;
+
+    if(*(Rdr+19)==0)
+
+        *(Banderas+1)=1;
+    else
+        *(Banderas+1)=0;
+
+        c=*(Registros+Rn)+*(Registros+Rm);
+
+    if(c>=255)
+    *(Banderas+2)=1;
+    else
+        *(Banderas+2)=0;
+
+    if((((Rn>=128)&(Rm>=128))&(*(Rdr+19)<128)) |(((Rn<128)&(Rm<128))&(*(Rdr+19)>=128)))
+     *(Banderas+3)=1;
+    else
+        *(Banderas+3)=0;
+}
+
 
